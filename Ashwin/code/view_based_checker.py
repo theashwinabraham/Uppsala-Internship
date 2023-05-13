06e2af70605118fcde053a284dd4b2bdcda74522
Ashwin Abraham <210050023@iitb.ac.in>
2023-05-14 02:42:39 +0530

    Added post to view based checker

--- Ashwin/code/view_based_checker.py
+++ Ashwin/code/view_based_checker.py
@@ -13,3 +13,40 @@ def is_bad(config: str) -> bool:
             num += 1
     return num <= 1
 
+I = {'0'} # I_1
+
+def post(S: set) -> set:
+    ret_set = set()
+    for config in S:
+        for i in range(len(config)):
+            if config[i] == '0':
+                ret_set.add(config[:i] + '1' + config[i + 1 :])
+            elif config[i] == '1':
+                for j in range(len(config)):
+                    if config[j] != '0' and config[j] != '1' and config[j] != '2' and j != i:
+                        break
+                else:
+                    ret_set.add(config[:i] + '3' + config[i + 1 : ])
+            elif config[i] == '3':
+                for j in range(len(config)):
+                    if config[j] == '1' and j != i:
+                        ret_set.add(config[:i] + '2' + config[i + 1 :])
+                        break
+                else:
+                    ret_set.add(config[:i] + '4' + config[i + 1 :])
+            elif config[i] == '2':
+                for j in range(len(config)):
+                    if config[j] == '4' and j != i:
+                        ret_set.add(config[:i] + '4' + config[i + 1 :])
+                        break
+            elif config[i] == '4':
+                for j in range(i):
+                    if config[j] != '0' and config[j] != '1':
+                        break
+                else:
+                    for j in range(i + 1, len(config)):
+                        if config[j] != '0' and config[j] != '1' and config[j] != '4':
+                            break
+                    else:
+                        ret_set.add(config[:i] + '0' + config[i + 1 :])
+    return ret_set
