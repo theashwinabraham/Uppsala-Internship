013fdc1153cdf32e95791e114d04a1786ee46c50
Ashwin Abraham <210050023@iitb.ac.in>
2023-05-14 01:54:42 +0530

    Uploaded Szymanski

---
+++ Ashwin/code/context_based_checker.py
@@ -0,0 +1,11 @@
+# Ashwin Abraham, 2023
+
+'''
+Currently, this checks only the non atomic version of Szymanski's protocol
+
+TODO: Parse arbitrary C/C++ programs as input
+'''
+
+'''
+Incomplete
+'''
---
+++ Ashwin/code/view_based_checker.py
@@ -0,0 +1,10 @@
+# Ashwin Abraham, 2023
+
+'''
+Currently, this checks only the atomic version of Szymanski's Protocol
+
+TODO: Parse C/C++ programs as input
+'''
+
+def is_bad(config: str) -> bool:
+    pass
